bb63acc95f82dfca5244ad91d5d86a99eb6b4fb8
Marc Harter <wavded@gmail.com>
2012-03-14 15:06:35 -0500

    Added zsh files

---
+++ zsh/aliases.zsh
@@ -0,0 +1,8 @@
+setopt auto_name_dirs
+setopt auto_pushd
+setopt pushd_ignore_dups
+
+alias ...='cd ../..'
+alias -- -='cd -'
+alias _='sudo'
+alias l='ls -lah'
---
+++ zsh/variables.zsh
@@ -0,0 +1,17 @@
+export PATH=/usr/local/bin:/usr/bin:/bin:/usr/local/sbin:/usr/sbin:/sbin:/usr/bin/core_perl:/usr/bin/vendor_perl:/usr/share/java/apache-ant/bin
+export TERM=xterm-256color
+export EDITOR=vim
+
+export GREP_OPTIONS='--color=auto'
+export GREP_COLOR='1;31'
+
+export LESS="-R"
+export LESSOPEN="| /usr/share/source-highlight/src-hilite-lesspipe.sh %s"
+export LESSHISTFILE=/dev/null
+export LESS_TERMCAP_mb=$'\E[01;32m'
+export LESS_TERMCAP_md=$'\E[01;32m'
+export LESS_TERMCAP_me=$'\E[0m'
+export LESS_TERMCAP_se=$'\E[0m'
+export LESS_TERMCAP_so=$'\E[01;44;33m'
+export LESS_TERMCAP_ue=$'\E[0m'
+export LESS_TERMCAP_us=$'\E[01;37m'
