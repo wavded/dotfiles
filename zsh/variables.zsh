67a17ae7ea4ca5372f6364ad0d53b14cbec81506
Marc Harter <wavded@gmail.com>
2012-03-29 14:33:37 -0500

    Removed LESS options, updated vim bundle

--- zsh/variables.zsh
+++ zsh/variables.zsh
@@ -4,14 +4,3 @@ export EDITOR=vim
 
 export GREP_OPTIONS='--color=auto'
 export GREP_COLOR='1;31'
-
-export LESS="-R"
-export LESSOPEN="| /usr/share/source-highlight/src-hilite-lesspipe.sh %s"
-export LESSHISTFILE=/dev/null
-export LESS_TERMCAP_mb=$'\E[01;32m'
-export LESS_TERMCAP_md=$'\E[01;32m'
-export LESS_TERMCAP_me=$'\E[0m'
-export LESS_TERMCAP_se=$'\E[0m'
-export LESS_TERMCAP_so=$'\E[01;44;33m'
-export LESS_TERMCAP_ue=$'\E[0m'
-export LESS_TERMCAP_us=$'\E[01;37m'
