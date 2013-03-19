6f84d219f79ed0d047c409b3b718292ea5c78708
Marc Harter <wavded@gmail.com>
2013-03-19 10:59:42 -0500

    added; edit command line

--- zsh/key-bindings.zsh
+++ zsh/key-bindings.zsh
@@ -28,3 +28,7 @@ bindkey '^?' backward-delete-char
 bindkey "^[[3~" delete-char
 bindkey "^[3;5~" delete-char
 bindkey "\e[3~" delete-char
+
+autoload -U edit-command-line
+zle -N edit-command-line
+bindkey '\C-x\C-e' edit-command-line
