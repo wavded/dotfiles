d95455e9ef6a5fab8dc93cf304d7d06f72bda666
Marc Harter <wavded@gmail.com>
2013-05-27 21:19:17 -0500

    updated; trying a new zsh prompt

--- zsh/variables.zsh
+++ zsh/variables.zsh
@@ -1,6 +1,7 @@
 export PATH=/usr/local/bin:/usr/bin:/bin:/usr/local/sbin:/usr/sbin:/sbin:/usr/bin/core_perl:/usr/bin/vendor_perl:/usr/share/java/apache-ant/bin
 export TERM=xterm-256color
 export EDITOR=vim
+export DEFAULT_USER=wavded
 
 export GREP_OPTIONS='--color=auto'
 export GREP_COLOR='1;31'
