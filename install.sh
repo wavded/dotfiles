fef815c0a74353bb637c55ca75e8b305de733d56
Marc Harter <wavded@gmail.com>
2016-05-17 09:54:44 -0500

    Update

--- install.sh
+++ install.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
-files=( zsh zshrc vim vimrc gitconfig hushlogin eslintrc tern-project )
+files=( zsh zshrc gitconfig hushlogin eslintrc tern-project )
 
 for f in "${files[@]}"
 do
   ln -fs "$PWD/$f" "$HOME/.$f";
 done
+
+# Neovim
+mkdir -p $HOME/.config
+ln -fs "$PWD/vim" "$HOME/.config/nvim";
 
 vim -S vim/snapshot.vim
