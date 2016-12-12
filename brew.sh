33e30af69c7969c6fcef45bf0263286a04ccdc89
Marc Harter <wavded@gmail.com>
2016-12-12 14:24:45 -0600

    Update brew install.

--- brew.sh
+++ brew.sh
@@ -17,16 +17,18 @@ brew install bash-git-prompt
 brew install tree
 brew install ripgrep
 brew install autojump
-brew install yarn
 brew install graphicsmagick
 brew install cairo
 brew install tmux
 brew install reattach-to-user-namespace
 
-brew install homebrew/versions/node6-lts
+brew install node@6
+brew install yarn
 brew install go
 
 brew cask install postico
 brew cask install docker
+
+yarn global install eslint babel-cli babel-eslint eslint-plugin-react tern istanbul nodemon
 
 brew cleanup
