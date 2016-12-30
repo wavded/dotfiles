f377df8e7b010dfd199e57eeeafa4a52bbb52c76
Marc Harter <wavded@gmail.com>
2016-12-29 23:23:24 -0600

    Add script to enable italics in vim/tmux.

---
+++ fix-terminfo.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+# Enable support for italics in vim/tmux
+
+TERMINFO=~/.terminfo
+rm -rf "$TERMINFO"
+
+tmp=$(mktemp)
+
+cat > "$tmp" <<EOF
+xterm-256color|xterm with 256 colors and italic,
+    kbs=\177,
+    sitm=\E[3m, ritm=\E[23m,
+    use=xterm-256color,
+tmux-256color|tmux with 256 colors and italic,
+    kbs=\177,
+    sitm=\E[3m, ritm=\E[23m,
+    smso=\E[7m, rmso=\E[27m,
+    use=screen-256color,
+EOF
+
+tic -x "$tmp"
