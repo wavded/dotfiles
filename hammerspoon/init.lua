568451d3ba65cf77756cbcb329ba3ddc342aa097
Marc Harter <wavded@gmail.com>
2019-04-25 12:07:20 -0500

    Better ESC handling for Hammerspoon

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,46 +1,36 @@
 -- Map CTRL, pressed alone, as ESC --
 
-send_escape = false
-last_mods = {}
-
-control_key_handler = function()
-  send_escape = false
-end
-
-control_key_timer = hs.timer.delayed.new(0.15, control_key_handler)
+ctrl_pressed = false
+key_pressed = false
 
 control_handler = function(evt)
-  local new_mods = evt:getFlags()
-  if last_mods["ctrl"] == new_mods["ctrl"] then
-    return false
-  end
-  if not last_mods["ctrl"] then
-    last_mods = new_mods
-    send_escape = true
-    control_key_timer:start()
+  local flags = evt:getFlags()
+  local key_code = evt:getKeyCode()
+
+  if flags.ctrl and key_code == 62
+    and not (flags.alt or flags.shift or flags.cmd or flags.fn)
+  then
+    ctrl_pressed = true
+    key_pressed = false
+  elseif ctrl_pressed
+    and not (flags.cmd or flags.alt or flags.shift or flags.fn)
+    and not key_pressed
+  then
+    ctrl_pressed = false
+    return true, {
+      hs.eventtap.event.newKeyEvent({}, 'escape', true),
+      hs.eventtap.event.newKeyEvent({}, 'escape', false),
+    }
   else
-    last_mods = new_mods
-    control_key_timer:stop()
-    if send_escape then
-      return true, {
-        hs.eventtap.event.newKeyEvent({}, 'escape', true),
-        hs.eventtap.event.newKeyEvent({}, 'escape', false),
-      }
-    end
+    key_pressed = true
   end
-  return false
 end
 
-control_tap = hs.eventtap.new({hs.eventtap.event.types.flagsChanged}, control_handler)
+control_tap = hs.eventtap.new(
+  {hs.eventtap.event.types.flagsChanged, hs.eventtap.event.types.keyUp},
+  control_handler
+)
 control_tap:start()
-
--- other_handler = function(evt)
---   send_escape = false
---   return false
--- end
-
--- other_tap = hs.eventtap.new({hs.eventtap.event.types.keyDown}, other_handler)
--- other_tap:start()
 
 -- Window Placement --
 
