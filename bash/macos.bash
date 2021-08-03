f0c154bdcfef672bb2215012ec86f1f434d64449
Marc Harter <wavded@gmail.com>
2021-08-03 14:59:55 -0500

    Pull cargo env out of bashrc and bash_profile

--- bash/macos.bash
+++ bash/macos.bash
@@ -17,6 +17,7 @@ export PATH=/usr/local/opt/node@14/bin:$PATH
 export PATH=/Applications/Postgres.app/Contents/Versions/latest/bin:$PATH
 export PATH="/usr/local/sbin:$PATH"
 export PATH="$HOME/.deno/bin:$PATH"
+. "$HOME/.cargo/env"
 
 # Disable per-terminal-session history: http://stackoverflow.com/a/34803825
 export SHELL_SESSION_HISTORY=0
