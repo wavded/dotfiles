cc4f2829f1678a8bf1928904640be67f84b262a1
Marc Harter <wavded@gmail.com>
2025-05-15 22:17:43 -0700

    WIP

--- nvim/lua/wavded/core/lang.lua
+++ nvim/lua/wavded/core/lang.lua
@@ -1,5 +1,3 @@
 vim.cmd(
   [[au FileType javascript,typescript,javascriptreact,typescriptreact map <leader>e :100split \| term NODE_ENV=test vitest run %<cr>]]
 )
-
-vim.cmd([[au FileType go map <leader>e :GoTestFile<cr>]])
