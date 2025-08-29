8be66bf98933d9369293d8dfff41aad1b2c5e6bf
Marc Harter <wavded@gmail.com>
2025-08-29 13:14:09 -0700

    Integrate mcphub.nvim with CodeCompanion
    Includes:
    
    - Add `ravitemer/mcphub.nvim` plugin
    - Configure `mcphub` extension for `codecompanion.nvim`
    - Enable `make_vars` and `make_slash_commands`

--- nvim/lua/wavded/plugins/codecompanion.lua
+++ nvim/lua/wavded/plugins/codecompanion.lua
@@ -1,5 +1,15 @@
 return {
   {
+    "ravitemer/mcphub.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    build = "npm install -g mcp-hub@latest", -- Installs `mcp-hub` node binary globally
+    opts = {
+      auto_approve = true,
+    },
+  },
+  {
     "olimorris/codecompanion.nvim",
     lazy = false,
     dependencies = {
@@ -15,6 +25,16 @@ return {
             end,
 
             user = "Me",
+          },
+        },
+      },
+      extensions = {
+        mcphub = {
+          callback = "mcphub.extensions.codecompanion",
+          opts = {
+            make_vars = true,
+            make_slash_commands = true,
+            show_result_in_chat = true,
           },
         },
       },
